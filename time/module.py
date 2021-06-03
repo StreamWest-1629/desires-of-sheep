b2f5f8229018593d8b851e71044adf0a8f18f8f6
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-04 08:33:43 +0900

    実行用のメソッドを作成

---
+++ time/module.py
@@ -0,0 +1,19 @@
+from place import GetPlace
+from ntp import GetUtcNow
+from timezone import GetTimeZone, ConvertFromUtc
+
+def GetTimeFromNet():
+    pos = GetPlace()
+    if pos is None:
+        return None
+    
+    tz = GetTimeZone(
+        latitude    = pos[0], 
+        longitude   = pos[1]
+    )
+
+    dt = GetUtcNow()
+
+    return ConvertFromUtc(dt, tz)
+
+# print(GetTimeFromNet())
