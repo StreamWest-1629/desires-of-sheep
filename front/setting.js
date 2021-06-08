d1461c3d5e6083363ae358b269a08773de535314
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-09 07:30:09 +0900

    一画面分の遷移しかしないようにする

--- front/setting.js
+++ front/setting.js
@@ -3,32 +3,36 @@
 // Standard Modules
 const electron = require('electron');
 const {app, BrowserWindow } = require('electron');
+const ipcMain = require('electron').ipcMain;
 
 let setWindow = null;
 
 exports.Open = function() {
-    setWindow = new BrowserWindow({
-        width: 600,
-        height: 400,
-        autoHideMenuBar: true,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        resizable: false,
-        fullscreenable: false,
-        fullscreen: false,
-        modal: false,
-        minimizable: false,
-        maximizable: false,
-        icon: __dirname + "/../img/icon.jpg",
-        title: __dirname + "/../img/icon.jpg"
-    });
 
-    setWindow.loadFile('front/setting.html');
-    setWindow.on('closed', function() {
-        electron.session.defaultSession.clearCache(() => {});
-        setWindow = null;
-    });
+    if (setWindow == null) {            
+        setWindow = new BrowserWindow({
+            width: 600,
+            height: 400,
+            autoHideMenuBar: true,
+            webPreferences: {
+                nodeIntegration: true
+            },
+            resizable: false,
+            fullscreenable: false,
+            fullscreen: false,
+            modal: false,
+            minimizable: false,
+            maximizable: false,
+            frame: false,
+            icon: __dirname + "/../img/icon.jpg"
+        });
 
-    app.on('window-all-closed', () => {});
+        setWindow.loadFile('front/setting.html');
+        setWindow.on('closed', function() {
+            electron.session.defaultSession.clearCache(() => {});
+            setWindow = null;
+        });
+
+        app.on('window-all-closed', () => {});
+    }
 }
