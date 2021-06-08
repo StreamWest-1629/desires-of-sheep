f4220845b6569b6e274317101b8e71cd638d9f89
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-08 20:27:29 +0900

    設定ウィンドウを作った

--- front/setting.js
+++ front/setting.js
@@ -7,5 +7,28 @@ const {app, BrowserWindow } = require('electron');
 let setWindow = null;
 
 exports.Open = function() {
-    
+    setWindow = new BrowserWindow({
+        width: 600,
+        height: 400,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: false,
+        fullscreenable: false,
+        fullscreen: false,
+        modal: false,
+        minimizable: false,
+        maximizable: false,
+        icon: __dirname + "/../img/icon.jpg",
+        title: __dirname + "/../img/icon.jpg"
+    });
+
+    setWindow.loadFile('front/setting.html');
+    setWindow.on('closed', function() {
+        electron.session.defaultSession.clearCache(() => {});
+        setWindow = null;
+    });
+
+    app.on('window-all-closed', () => {});
 }
