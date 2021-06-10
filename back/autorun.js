55b51ec811c9866d9de2a5b7039dd37a028b50b8
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-10 23:53:20 +0900

    関数から直接実行に変更した

--- back/autorun.js
+++ back/autorun.js
@@ -1,26 +1,24 @@
 const { app } = require('electron');
 const auto = require('auto-launch');
 
-exports.CheckRun = function() {
-
-    const doubleBoot = app.requestSingleInstanceLock();
-    if (!doubleBoot) {
-        app.quit();
-    }
-
-    var stokerRun = new auto({
-        name: 'Sheep\'s Stoking',
-        path: app.getPath('exe'),
-    })
-
-    stokerRun.isEnabled().then(function(isEnabled) {
-        if (isEnabled) {
-            return;
-        } else {
-            // stokerRun.enable();
-        }
-    })
-    .catch(function(err) {
-        // Todo: エラー時の動作
-    })
+const doubleBoot = app.requestSingleInstanceLock();
+if (!doubleBoot) {
+    app.quit();
 }
+
+var stokerRun = new auto({
+    name: 'Sheep\'s Stoking',
+    path: app.getPath('exe'),
+})
+
+stokerRun.isEnabled().then(function(isEnabled) {
+    if (isEnabled) {
+        return;
+    } else {
+        // stokerRun.enable();
+    }
+})
+.catch(function(err) {
+    // Todo: エラー時の動作
+})
+
