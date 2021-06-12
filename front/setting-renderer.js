9e9d5574b4e407c0a352c266a770f3e98274911e
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-12 11:42:13 +0900

    レンダラーのページ別呼び出し層の呼び出し方法を変更

--- front/setting-renderer.js
+++ front/setting-renderer.js
@@ -1,14 +1,27 @@
 const electron = require('electron');
 const { ipcRenderer } = require('electron');
 const log = require('electron-log');
+const timespan = require('timespan');
+const { TimeSpan } = require('timespan');
 
-exports.ReceiveValue = function(event, args) {};
-exports.SendValue = function() {return { mode:"initialize" };};
+var SendValue
+var ReceiveValue
 
-exports.Initializer = function() {
+exports.Initializer = function(recieve, send) {
     
+    SendValue = send;
+    ReceiveValue = recieve;
+
     // ipc Receiver
-    ipcRenderer.on("update", function(event, args){ ReceiveValue(event, args) });
+    ipcRenderer.on("update-reply", function(event, args){ 
+        log.log("ipc-Renderer reached.")
+        // log.log(args);
+        // var time = new TimeSpan(args['sleep-time']);
+        // var span = new TimeSpan(args['sleep-span']);
+        // const timestr = ('00' + time.hours).slice(-2) + ":" + ('00' + time.minutes).slice(-2);
+        // document.querySelector('label[for="sleep-time"]').textContent = timestr;
+        ReceiveValue(event, args) 
+    });
 
     // Initialize
     ipcRenderer.send("update", { mode:"initialize" });
@@ -22,8 +35,6 @@ function onclose() {
     ipcRenderer.send("kill");
 }
 function onsave() {
+    log.log('onsave() reached.')
     ipcRenderer.send("update", SendValue());
-}
-function SettingSave() {
-    // 
 }
