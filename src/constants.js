df42e149975646b0940cd454904132ed5a25ff97
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-18 14:14:21 +0900

    ウィンドウを開けるようにした

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const iconPath = `${__dirname}/../img/icon.jpg`;
+const htmlPath = {
+    settings: 'src/html/settings.html',
+    musics: 'src/html/musics.html',
+    about: 'src/html/about.html'
+};
+
+exports.iconPath = iconPath;
+exports.htmlPath = htmlPath;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const { app, Tray, BrowswerWindow, session } = require('electron');
-const iconPath = `${__dirname}/../img/icon.jpg`;
+const { app, Tray, BrowserWindow, session } = require('electron');
+const { iconPath, htmlPath } = require('./constants');
 
-let tray;
+const tooltipStr = '希望の睡眠 - 設定を開く';
+
+
+let tray = null;
+let win = null;
 
 app.on('window-all-closed', () => {
     session.defaultSession.clearCache(() => {});
@@ -11,4 +15,40 @@ app.on('window-all-closed', () => {
 
 app.on('ready', () => {
     tray = new Tray(iconPath);
+    tray.on('double-click', () => { OpenWindow(); });
 });
+
+function OpenWindow() {
+    win = new BrowserWindow({
+        width: 600,
+        height: 430,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }, 
+        resizable: false,
+        fullscreenable: false,
+        fullscreen: false,
+        modal: false,
+        minimizable: false,
+        maximizable: false,
+        frame: false,
+        icon: iconPath
+    });
+
+    win.loadFile(htmlPath.settings);
+    win.webContents.on('will-navigate', (event, url) => {
+        event.preventDefault();
+        shell.openExternal(url);
+    })
+    
+    win.webContents.on('new-window', (event, url) => {
+        event.preventDefault();
+        shell.openExternal(url);
+    })
+
+    win.on('closed', function() {
+        settingsWindow = null;
+    });
+}
